f51654a4bcab3cd6861a9ad1b96f69ac71772aae
Pablo Carranza Vélez <pablocarranza@gmail.com>
2013-03-23 12:40:46 -0300

    Rails new...

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mercadoanalytics::Application.config.secret_token = '4187c7e2137adf23e1d68c76e0da2d5ca0d5afbbc273d0e886cfb6b7f40da5fca45010217d6e8fa9cc921ef6cb7cc2be2a91ccbe5e96cddd66431a139d90c07d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Mercadoanalytics::Application.config.session_store :cookie_store, key: '_mercadoanalytics_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Mercadoanalytics::Application.config.session_store :active_record_store
