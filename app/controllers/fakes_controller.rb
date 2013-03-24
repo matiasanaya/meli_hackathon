a5a0016a2f3796e4b619073b22708e0f5222aaaa
Matias Anaya <matiasanaya@gmail.com>
2013-03-24 07:08:43 -0300

    More tweaking

--- app/controllers/fakes_controller.rb
+++ app/controllers/fakes_controller.rb
@@ -1,6 +1,6 @@
 class FakesController < ApplicationController
 	def test
-		@item = Item.new(has_stock: [true, false].sample, meli_stock: (rand(63) + 1), meli_title:'Lorem ipsum fsdi ceowo csjo coscosko csoacs', stock_tracked: [false].sample, real_stock: 5)
+		@item = Item.new(has_stock: [true, false].sample, meli_stock: (rand(63) + 1), meli_title:'Lorem ipsum fsdi ceowo csjo coscosko csoacs', stock_tracked: [true].sample, real_stock: 5)
 		@item.save
 		@item
 	end
